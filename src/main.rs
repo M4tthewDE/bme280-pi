b702db6dee3648e1f659c067856447c57d0a35d9
M4tthewDE <mattider2@gmail.com>
2021-07-11 20:12:22 +0200

    finished oversample setting for humidty register

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use rppal::i2c::I2c;
 
+const REG_CONTROL: u8 = 0xF4;
+
 const REG_CONTROL_HUM: u8 = 0xF2;
 const OVERSAMPLE_HUM: u8 = 2;
+
+const MODE: u8 = 1;
 
 fn main() {
     let mut i2c = I2c::with_bus(1).unwrap();
@@ -12,6 +16,12 @@ fn main() {
 
     // oversample setting for humidity register - page 26
     match i2c.write(&[REG_CONTROL_HUM, OVERSAMPLE_HUM]) {
+        Ok(_) => {println!("Set oversample setting for humidity register.")},
+        Err(error) => {println!("Failed to set  oversample setting for humidity register: {:?}", error)}
+    } 
+
+    let control = OVERSAMPLE_HUM<<5 | OVERSAMPLE_HUM<<2| MODE;
+    match i2c.write(&[REG_CONTROL, control]) {
         Ok(_) => {println!("Set oversample setting for humidity register.")},
         Err(error) => {println!("Failed to set  oversample setting for humidity register: {:?}", error)}
     } 
