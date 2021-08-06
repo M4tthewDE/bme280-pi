2996e23de849cb8c3e50130e2e7570dce8f26707
M4tthewDE <mattider2@gmail.com>
2021-08-06 19:36:29 +0200

    use library you dont always have to do everything yourself dumbo

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,16 @@
-use rppal::i2c::I2c;
-
-const REG_CONTROL: u8 = 0xF4;
-
-const REG_CONTROL_HUM: u8 = 0xF2;
-const OVERSAMPLE_HUM: u8 = 2;
-
-const MODE: u8 = 1;
+use linux_embedded_hal::{Delay, I2cdev};
+use bme280::BME280;
 
 fn main() {
-    let mut i2c = I2c::with_bus(1).unwrap();
-    match i2c.set_slave_address(0x76) {
-        Ok(_) => {println!("Set slave adress.")},
-        Err(error) => {println!("Failed to set adress: {:?}", error)}
-    }
 
-    // oversample setting for humidity register - page 26
-    match i2c.write(&[REG_CONTROL_HUM, OVERSAMPLE_HUM]) {
-        Ok(_) => {println!("Set oversample setting for humidity register.")},
-        Err(error) => {println!("Failed to set  oversample setting for humidity register: {:?}", error)}
-    } 
+    let i2c_bus = I2cdev::new("/dev/i2c-1").unwrap();
+    let mut bme280 = BME280::new_primary(i2c_bus, Delay);
 
-    let control = OVERSAMPLE_HUM<<5 | OVERSAMPLE_HUM<<2| MODE;
-    match i2c.write(&[REG_CONTROL, control]) {
-        Ok(_) => {println!("Set oversample setting for humidity register.")},
-        Err(error) => {println!("Failed to set  oversample setting for humidity register: {:?}", error)}
-    } 
+    bme280.init().unwrap();
+
+    let measurements = bme280.measure().unwrap();
+
+    println!("Relative Humidity = {}%", measurements.humidity);
+    println!("Temperature = {} deg C", measurements.temperature);
+    println!("Pressure = {} pascals", measurements.pressure);
 }
