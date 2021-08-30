3bcadd1bdde4c7d3d84eec9fb975a66c52c98d6b
M4tthewDE <mattider2@gmail.com>
2021-08-30 13:22:42 +0200

    remove retrival of data with cmd line args - breaking change: only retrieval by prometheus api from now on

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
 use linux_embedded_hal::{Delay, I2cdev};
 use bme280::BME280;
-use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
     let i2c_bus = I2cdev::new("/dev/i2c-1").unwrap();
     let mut bme280 = BME280::new_primary(i2c_bus, Delay);
 
@@ -19,11 +16,4 @@ fn main() {
         ", measurements.temperature, measurements.humidity).as_bytes()).unwrap();
 
     file.flush().unwrap();
-
-    match args[1].as_str() {
-        "temp" => println!("{:.2}", measurements.temperature),
-        "pressure" => println!("{:.2}", measurements.pressure),
-        "humidity" => println!("{:.2}", measurements.humidity),
-        _ => {}
-    }
 }
